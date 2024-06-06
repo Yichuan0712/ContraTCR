4debeecbf9518481a70c2ff6d569d435497186d9
Yichuan <1758783038@qq.com>
2024-06-06 16:11:46 -0500

    Commit and Push

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def main(parse_args, configs, valid_fold_number, test_fold_number):
 
     torch.cuda.empty_cache()
     curdir_path, result_path, checkpoint_path, log_path = prepare_saving_dir(parse_args)
-    print(curdir_path, result_path, checkpoint_path, log_path)
+    printl(f"{'='*80}\n", curdir_path, f"{'='*80}\n", log_path=log_path)
     return
 
 
