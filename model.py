95811170f416cb1192e9fc56f706778bf3fc489b
Yichuan <1758783038@qq.com>
2024-06-09 17:39:57 -0500

    Commit and Push

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer
 def get_tokenizer(configs):
-    if 'esm2' in configs.encoder_name:
-        tokenizer = AutoTokenizer.from_pretrained(configs.encoder.model_name)
+    if 'facebook/esm2' in configs.encoder_name:
+        tokenizer = AutoTokenizer.from_pretrained(configs.encoder_name)
     else:
         raise ValueError("Wrong tokenizer specified.")
     return tokenizer
