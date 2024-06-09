001b124c084e910a8fb6db12871db0b6bffbcc9c
Yichuan <1758783038@qq.com>
2024-06-09 16:01:30 -0500

    Commit and Push

--- data.py
+++ data.py
@@ -1,21 +1,28 @@
 from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 import pandas as pd
 
+class miniDataset(Dataset):
+    def __init__(self):
+        return
 
-def prepare_dataloader(configs, valid_fold_index, test_fold_index):
+
+def get_dataloader(configs, valid_fold_index, test_fold_index):
     if configs.dataset == "mini":
-        def load_data_mini(fold_index):
+        def get_dataframe_mini(fold_index):
             # Load the data from CSV files
             dataframe = pd.read_csv(f'./dataset/mini/minifold_{fold_index}.csv')
             return dataframe
+
         # Load validation and test data
-        valid_data = load_data_mini(valid_fold_index)
-        test_data = load_data_mini(test_fold_index)
+        valid_data = get_dataframe_mini(valid_fold_index)
+        test_data = get_dataframe_mini(test_fold_index)
 
         # Combine and shuffle remaining data for training
-        train_data = pd.concat([load_data_mini(i) for i in range(5) if i not in [valid_fold_index, test_fold_index]],
+        train_data = pd.concat([get_dataframe_mini(i) for i in range(5) if i not in [valid_fold_index, test_fold_index]],
                                ignore_index=True)
         train_data = train_data.sample(frac=1).reset_index(drop=True)
+        # return all rows, avoid inserting the old index as a column in the new DataFrame
 
         # Create datasets
         train_dataset = miniDataset(train_data)
